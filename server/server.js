3fd8d4e34e0c8083ef2f700a6c94c677586d2461
arshavskiy <pasha.websbee@gmail.com>
2017-12-12 21:25:56 +0200

    npm ng2-file-upload installed

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ var storage = multer.diskStorage({ //multers disk storage settings
     },
     filename: function (req, file, cb) {
         var datetimestamp = Date.now();
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
     }
 });
 var upload = multer({ //multer settings
